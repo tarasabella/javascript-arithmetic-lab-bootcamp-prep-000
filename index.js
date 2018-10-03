25b5c62d2a5dfd191ce2a5694250c1ad182b381f
Tara E. Sabella <tarasabella@gmail.com>
2018-10-03 16:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function makeInt(a) {
   return parseInt(a, 10)
 }
 function preserveDecimal(a) {
-  return parseFloat(parseFloat(a));
+  return parseFloat(a);
 }
 function preserveDecimal(a) {
   return parseInt("hey", 10)
