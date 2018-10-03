5ca64f0595100ab130862079e21af88290a6b612
Tara E. Sabella <tarasabella@gmail.com>
2018-10-03 16:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a, b) {
 function divide(a, b) {
   return a /= b
 }
-function inc(a) {
-  return (a++)
+function inc(n) {
+  return n++;
 }
-function dec(a) {
-  return (a--)
+function dec(n) {
+  return n--;
 }
